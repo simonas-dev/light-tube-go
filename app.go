e24c800d37448c2b5372c17181a20cf4f098ba00
Simonas Sankauskas <simonas.sank@gmail.com>
2017-03-30 19:09:39 +0000

    Good led algo:

--- app.go
+++ app.go
@@ -7,41 +7,88 @@ import (
 	"github.com/simonassank/aubio-go"
 	"github.com/simonassank/go_ws2811"
 	"math"
+	"time"
 )
 
+// Configs
 var (
 	srcPath    = flag.String("src", "", "Path to source file. Required")
 	Samplerate = flag.Int("samplerate", 44100, "Sample rate to use for the audio file")
-	Blocksize  = flag.Int("blocksize", 256, "Blocksize use for the audio file")
-	Bufsize    = flag.Int("bufsize", 512, "Bufsize use for the audio file")
+	Blocksize  = flag.Int("blocksize", 512, "Blocksize use for the audio file")
+	Bufsize    = flag.Int("bufsize", 1024, "Bufsize use for the audio file")
 	Silence    = flag.Float64("silence", -90.0, "Threshold to use when detecting silence")
 	Threshold  = flag.Float64("threshold", 0.0, "Detection threshold")
 	Verbose    = flag.Bool("verbose", false, "Print verbose output")
 	help       = flag.Bool("help", false, "Print this help")
+	LedCount   = 144
 )
 
+// Final vars
+var (
+	A4 = 440.0
+	C0 = A4 * math.Pow(2, -4.75)
+	colors = []uint32 {
+		uint32(0xFF0000),
+		uint32(0xFF0000),
+		uint32(0xFFFF00),
+		uint32(0xFFFF00),
+		uint32(0xC3F2FF),
+		uint32(0x7F8BFD),
+		uint32(0x7F8BFD),
+		uint32(0xF37900),
+		uint32(0xF37900),
+		uint32(0x33CC33),
+		uint32(0x33CC33),
+		uint32(0x8EC9FF),
+	}
+)
+
+// Non final vars
+var (
+ratio float64
+energies []float64
+fftgrain *aubio.ComplexBuffer
+inputBuffer *aubio.SimpleBuffer
+led_index int
+led_colors = make([]uint32, LedCount)
+buff = make([]float64, int(*Bufsize))
+)
 
 func main() {
 	fmt.Println("Go!")
 
-	ws2811.Init(18, 144, 255)
+	ws2811.Init(18, LedCount, 255)
 
-	c, _ := alsa.NewCaptureDevice(
+	c, errC := alsa.NewCaptureDevice(
 		"plughw:CARD=Set,DEV=0",
-		 2,
+		 1,
 		 alsa.FormatFloat64LE,
 		 *Samplerate,
-		 alsa.BufferParams{},
+		 alsa.BufferParams{
+			*Samplerate,
+			1,
+			1,
+		 },
 	)
 
-	p, _ := alsa.NewPlaybackDevice(
+	fmt.Println(errC)
+
+
+	p, errP := alsa.NewPlaybackDevice(
 		"plughw:CARD=Set,DEV=0",
-		 2,
+		 1,
 		 alsa.FormatFloat64LE,
 		 *Samplerate,
-		 alsa.BufferParams{},
+		 alsa.BufferParams{
+			*Samplerate,
+			1,
+			1,
+		 },
 	)
-	
+
+	fmt.Println(errP)
+
+
 	pitch := aubio.NewPitch(
 		aubio.PitchDefault,
 		uint(*Bufsize),
@@ -55,56 +102,58 @@ func main() {
 	fb := aubio.NewFilterBank(40, uint(*Bufsize))
 	fb.SetMelCoeffsSlaney(uint(*Samplerate))
 
-	b4 := make([]float64, 512)
-
-	calc_outs := 0
-	audio_outs := 0
-
 	_ = phVoc
-	_ = calc_outs
-	
+
+
+	_ = ratio
+	_ = energies
+	_ = fftgrain
+	_ = inputBuffer
+	_ = led_index
+	_ = led_colors
+	_ = buff
+
 	go func() {
-		var (
-			ratio float64
-			energies []float64
-			fftgrain *aubio.ComplexBuffer
-			inputBuffer *aubio.SimpleBuffer
-			led_index int
-			led_colors = make([]uint32, 144)
-		)
-		
 		for {
-			inputBuffer = aubio.NewSimpleBufferData(512, b4)
-			pitch.Do(inputBuffer)
-			_ = pitch.Buffer().Slice()[0]
-
-			phVoc.Do(inputBuffer)
-			fftgrain = phVoc.Grain()
-			fb.Do(fftgrain)
-			energies = fb.Buffer().Slice()
-
-			for led_index = 0; led_index < 144; led_index++ {
-				ratio = energies[int(Round(float64(led_index)/3.6))]
-				// ratio = math.Pow(ratio, 1.5)
-				ratio = ratio * 30.0
-				// ratio = math.Pow(ratio, 1.5)
-				// fmt.Println(ratio)
-				if ratio > 1 {
-					ratio = 1
-				}
-
-				led_colors[led_index] = AvgColor(int(led_colors[led_index]), int(GetColor(ratio)))
-				led_colors[led_index] = AvgColor(int(led_colors[led_index]), 0)
-			}
-			ws2811.SetBitmap(led_colors)
-			ws2811.Render()
-		}
+			fmt.Println("audio")
+			c.Read(buff)
+			p.Write(buff)
+		}	
 	}()
 
 	for {
-		_, _ = c.Read(b4)
-		_, _ = p.Write(b4)
-		audio_outs += 1
+
+		start := time.Now()
+		inputBuffer = aubio.NewSimpleBufferData(uint(*Bufsize), buff)
+		pitch.Do(inputBuffer)
+		pitch_val := pitch.Buffer().Slice()[0]
+		color := colors[GetNoteIndex(pitch_val)]
+
+		fmt.Println(color)
+
+		phVoc.Do(inputBuffer)
+		fftgrain = phVoc.Grain()
+		fb.Do(fftgrain)
+		energies = fb.Buffer().Slice()
+
+		for led_index = 0; led_index < LedCount; led_index++ {
+			ratio = energies[int(Round(float64(led_index)/3.6))]
+			ratio = math.Pow(ratio, 1)
+			// ratio *= 15
+			// ratio = math.Pow(ratio, 4)
+			if ratio > 0.8 {
+				ratio = 0.8
+			}
+			
+			tinted := AvgColor(int(color), int(0x00ffff), 0.5)
+			led_colors[led_index] = AvgColor(int(led_colors[led_index]), int(tinted), ratio)
+			led_colors[led_index] = AvgColor(int(led_colors[led_index]), 0, 0.05)
+			ws2811.SetLed(led_index, led_colors[led_index])
+		}
+		
+		// fmt.Println(led_colors)
+		ws2811.Render()
+		fmt.Println(time.Since(start))
 	}
 }
 
@@ -114,22 +163,24 @@ func GetColor(ratio float64) uint32 {
 }
 
 func Round(f float64) float64 {
-    return math.Floor(f + .5)
+	return math.Floor(f + .5)
 }
 
-func AvgColor(a int, b int) uint32{
-	return uint32(float64(GetRed(a)+GetRed(b)) / 2.0 + float64(GetGreen(a)+GetGreen(b)) / 2.0 + float64(GetBlue(a)+GetBlue(b)) / 2)
+func AvgColor(a int, b int, ratio float64) uint32{
+	inv_ratio := 1-ratio
+	c_a := GetColorNum(a)
+	c_b := GetColorNum(b)
+	return uint32(int((c_a[0]*inv_ratio)+(c_b[0]*ratio)) << 16 + int((c_a[1]*inv_ratio)+(c_b[1]*ratio)) << 8 + int((c_a[2]*inv_ratio)+(c_b[2]*ratio)))
 }
 
-func GetRed(color int) int {
-	return color & 0xFF0000 >> 16
+func GetColorNum(color int) []float64 {
+	return []float64{float64(color & 0xFF0000 >> 16), float64(color & 0xFF00 >> 8), float64(color & 0xFF)}
 }
 
-func GetBlue(color int) int {
-	return color & 0xFF00 >> 8
+func GetNoteIndex(freqHz float64) int {
+	div := freqHz/C0
+	if div == 0 { return 0 }
+	h := int(Round(12 * math.Log2(div)))
+	n := h % 12
+	return int(n)
 }
-
-func GetGreen(color int) int {
-	return color & 0xFF
-}
-
---
+++ led.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    _"fmt"
+    "github.com/simonassank/go_ws2811"
+)
+
+func main() {
+    ws2811.Init(18, 144, 100)
+    
+    for {
+        led_colors := make([]uint32, 144)
+
+        for led_index := 0; led_index < 144; led_index++ {
+            led_colors[led_index] = 0x0000ff
+            ws2811.SetBitmap(led_colors)
+            ws2811.Render()
+        }
+    }
+}
