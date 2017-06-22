ac36ce60c86bc57b8b8e2bf5133d6701ec44e01c
Simonas <simonas.sank@gmail.com>
2017-06-22 16:52:18 +0000

    Revert

--- app.go
+++ app.go
@@ -1,112 +1,122 @@
 package main
 
 import (
-	"fmt"
-	"flag"
-	"github.com/simonassank/go_ws2811"
-	"math"
-	"time"
-	"./config"
-	"./utils"
-	"./audio"
+    "fmt"
+    "github.com/simonassank/go_ws2811"
+    "math"
+    "time"
+    "./config"
+    "./utils"
+    "./audio"
 )
 
 // Non final vars
 var (
-	ratio float64
-	led_index int
-	led_colors = make([]uint32, LedCount)
-	buff = make([]float64, int(*Bufsize))
-	eq = make([]int, LedCount)
-	tinted uint32
-	inv_led_index int
-	configData config.Config
+    ratio           float64
+    led_index       int
+    tinted          uint32
+    inv_led_index   int
+    configData      config.Config
+    led_colors      = make([]uint32, LedCount)
+    buff            = make([]float64, int(1024))
+    eq              = make([]int, LedCount)
+    LedCount        = 144
 )
 
 func main() {
-	fmt.Println("Go!")
+    fmt.Println("Go!")
 
-	ws2811.Init(18, LedCount, 255)
+    ws2811.Init(18, LedCount, 255)
 
-	c, p := audio.NewAudio()
+    c, p := audio.NewAudio()
 
-	go func() {
-		for {
-			c.Read(buff)
-			p.Write(buff)
-		}	
-	}()
+    go func() {
+        for {
+            c.Read(buff)
+            p.Write(buff)
+        }
+    }()
 
-	go func() {
-		for {
-			energies, pitch_val := audio.GetEnergies(buff)
+    go func() {
+        var (
+            channel_led_count = LedCount/2
+            led_divider             = float64(channel_led_count)/40.0
+            channel_1_start         = 0
+            channel_1_end           = channel_led_count
+            channel_2_start         = channel_led_count
+            channel_2_end           = LedCount-1
+            energies                = make([]float64, int(40))
+            pitch_val               float64
+            color                   uint32
+            inv_led_index           int
+        )
 
-			color := utils.GetFloatColor(configData.Note_Colors, utils.GetNoteIndex(pitch_val))
+        for {
+            energies, pitch_val = audio.GetAnalaysis(buff)
 
-			channel_led_count := LedCount/2
-			led_divider := float64(channel_led_count)/40.0
-			channel_1_start := 0
-			channel_1_end := channel_led_count
-			channel_2_start := channel_led_count
-			channel_2_end := LedCount-1
+            color = utils.GetFloatColor(configData.Note_Colors, utils.GetNoteIndex(pitch_val))
 
-			fmt.Println(energies)
+            fmt.Println(energies)
 
-			// Channel 1
-			for led_index = channel_1_start; led_index < channel_1_end; led_index++ {
-				ratio = utils.GetEnergy(energies, float64(led_index)/led_divider)
-				ratio = math.Pow(ratio, configData.Pre_power)
-				ratio *= configData.Multi
-				ratio = math.Pow(ratio, configData.Post_power)
-				if ratio > configData.Max_level {
-					ratio = configData.Max_level
-				} else if (ratio < configData.Min_level) {
-					ratio = 0
-				}
+            // Channel 1
+            for led_index = channel_1_start; led_index < channel_1_end; led_index++ {
+                ratio = utils.GetEnergy(energies, float64(led_index)/led_divider)
+                ratio = math.Pow(ratio, configData.Pre_power)
+                ratio *= configData.Multi
+                ratio = math.Pow(ratio, configData.Post_power)
+                if ratio > configData.Max_level {
+                    ratio = configData.Max_level
+                } else if (ratio < configData.Min_level) {
+                    ratio = 0
+                }
 
-				inv_led_index := (channel_led_count)-led_index
-				eq[inv_led_index] = int(ratio * 100)
+                inv_led_index = (channel_led_count)-led_index
+                eq[inv_led_index] = int(ratio * 100)
 
-				tinted = utils.AvgColor(int(color), int(configData.Tint_color), configData.Tint_alpha)
-				led_colors[inv_led_index] = utils.AvgColor(int(led_colors[inv_led_index]), int(tinted), ratio)
-				led_colors[inv_led_index] = utils.AvgColor(int(led_colors[inv_led_index]), 0, configData.Fade_ratio)
-				ws2811.SetLed(inv_led_index, led_colors[inv_led_index])
-			}
+                tinted = utils.AvgColor(int(color), int(configData.Tint_color), configData.Tint_alpha)
+                led_colors[inv_led_index] = utils.AvgColor(int(led_colors[inv_led_index]), int(tinted), ratio)
+                led_colors[inv_led_index] = utils.AvgColor(int(led_colors[inv_led_index]), 0, configData.Fade_ratio)
+                ws2811.SetLed(inv_led_index, led_colors[inv_led_index])
+            }
 
-			// Channel 2
-			for led_index = channel_2_start; led_index < channel_2_end; led_index++ {
-				ratio = utils.GetEnergy(energies, float64(led_index-channel_2_start)/led_divider)
-				ratio = math.Pow(ratio, configData.Pre_power)
-				ratio *= configData.Multi
-				ratio = math.Pow(ratio, configData.Post_power)
-				if ratio > configData.Max_level {
-					ratio = configData.Max_level
-				} else if (ratio < configData.Min_level) {
-					ratio = 0
-				}
-				eq[led_index] = int(ratio * 100)
+            // Channel 2
+            for led_index = channel_2_start; led_index < channel_2_end; led_index++ {
+                ratio = utils.GetEnergy(energies, float64(led_index-channel_2_start)/led_divider)
+                ratio = math.Pow(ratio, configData.Pre_power)
+                ratio *= configData.Multi
+                ratio = math.Pow(ratio, configData.Post_power)
+                if ratio > configData.Max_level {
+                    ratio = configData.Max_level
+                } else if (ratio < configData.Min_level) {
+                    ratio = 0
+                }
+                eq[led_index] = int(ratio * 100)
 
-				tinted = utils.AvgColor(int(color), int(configData.Tint_color), configData.Tint_alpha)
-				led_colors[led_index] = utils.AvgColor(int(led_colors[led_index]), int(tinted), ratio)
-				led_colors[led_index] = utils.AvgColor(int(led_colors[led_index]), 0, configData.Fade_ratio)
-				ws2811.SetLed(led_index, led_colors[led_index])
-			}
+                tinted = utils.AvgColor(int(color), int(configData.Tint_color), configData.Tint_alpha)
+                led_colors[led_index] = utils.AvgColor(int(led_colors[led_index]), int(tinted), ratio)
+                led_colors[led_index] = utils.AvgColor(int(led_colors[led_index]), 0, configData.Fade_ratio)
+                ws2811.SetLed(led_index, led_colors[led_index])
+            }
 
-			ws2811.Render()
-		}
-	}()
+            ws2811.Render()
+        }
+    }()
 
-	go func() {
-		for {
-			newConfig, err := config.Load()
-			if (err == nil) {
-				configData = newConfig
-			}
-			time.Sleep(1000)
-		}
-	}()
-	
-	for {
-		time.Sleep(100)
-	}
+    go func() {
+        var (
+            newConfig config.Config
+            err error
+        )
+        for {
+            newConfig, err = config.Load()
+            if (err == nil) {
+                configData = newConfig
+            }
+            time.Sleep(1000)
+        }
+    }()
+    
+    for {
+        time.Sleep(100)
+    }
 }
--- audio/audio.go
+++ audio/audio.go
@@ -1,76 +1,76 @@
 package audio
 
 import (
-	"github.com/cocoonlife/goalsa"
-	"github.com/simonassank/aubio-go"
-	"fmt"
-	"flag"
+    "github.com/cocoonlife/goalsa"
+    "github.com/simonassank/aubio-go"
+    "fmt"
+    "flag"
 )
 
 var (
-	SrcPath     = flag.String("src", "", "Path to source file. Required")
-	Samplerate  = flag.Int("samplerate", 44100, "Sample rate to use for the audio file")
-	Blocksize   = flag.Int("blocksize", 512, "Blocksize use for the audio file")
-	Bufsize     = flag.Int("bufsize", 1024, "Bufsize use for the audio file")
-	Silence     = flag.Float64("silence", -90.0, "Threshold to use when detecting silence")
-	Threshold   = flag.Float64("threshold", 0.0, "Detection threshold")
-	Verbose     = flag.Bool("verbose", false, "Print verbose output")
-	help        = flag.Bool("help", false, "Print this help")
-	LedCount    = 144
+    SrcPath     = flag.String("src", "", "Path to source file. Required")
+    Samplerate  = flag.Int("samplerate", 44100, "Sample rate to use for the audio file")
+    Blocksize   = flag.Int("blocksize", 512, "Blocksize use for the audio file")
+    Bufsize     = flag.Int("bufsize", 1024, "Bufsize use for the audio file")
+)
+
+var (
+    pitch       *aubio.Pitch
+    phVoc       *aubio.PhaseVoc
+    fb          *aubio.FilterBank
+    inBuff      *aubio.SimpleBuffer
 )
 
 func NewAudio() (c *alsa.CaptureDevice, p *alsa.PlaybackDevice) {
-	c, errC := alsa.NewCaptureDevice(
-		"plughw:CARD=Device,DEV=0",
-		2,
-		alsa.FormatFloat64LE,
-		*Samplerate,
-		alsa.BufferParams{
-			*Samplerate,
-			1,
-			1,
-		 },
-	)
+    c, errC := alsa.NewCaptureDevice(
+        "plughw:CARD=Device,DEV=0",
+        2,
+        alsa.FormatFloat64LE,
+        *Samplerate,
+        alsa.BufferParams{
+            *Samplerate,
+            1,
+            1,
+         },
+    )
 
-	fmt.Println(errC)
+    fmt.Println(errC)
 
-	p, errP := alsa.NewPlaybackDevice(
-		"plughw:CARD=Device,DEV=0",
-		2,
-		alsa.FormatFloat64LE,
-		*Samplerate,
-		alsa.BufferParams{
-			*Samplerate,
-			1,
-			1,
-		},
-	)
+    p, errP := alsa.NewPlaybackDevice(
+        "plughw:CARD=Device,DEV=0",
+        2,
+        alsa.FormatFloat64LE,
+        *Samplerate,
+        alsa.BufferParams{
+            *Samplerate,
+            1,
+            1,
+        },
+    )
 
-	fmt.Println(errP)
+    fmt.Println(errP)
 
-	return c, p
+
+    pitch = aubio.NewPitch(
+        aubio.PitchDefault,
+        uint(*Bufsize),
+        uint(*Blocksize),
+        uint(*Samplerate),
+    )
+    pitch.SetUnit(aubio.PitchOutFreq)
+    pitch.SetTolerance(0.9)
+
+    phVoc, _ = aubio.NewPhaseVoc(uint(*Bufsize), uint(*Blocksize))
+    fb = aubio.NewFilterBank(40, uint(*Bufsize))
+    fb.SetMelCoeffsSlaney(uint(*Samplerate))
+
+    return c, p
 }
 
 func GetAnalaysis(buffer []float64) ([]float64, float64) {
-	pitch := aubio.NewPitch(
-		aubio.PitchDefault,
-		uint(*Bufsize),
-		uint(*Blocksize),
-		uint(*Samplerate),
-	)
-	pitch.SetUnit(aubio.PitchOutFreq)
-	pitch.SetTolerance(0.9)
-
-	phVoc, _ := aubio.NewPhaseVoc(uint(*Bufsize), uint(*Blocksize))
-	fb := aubio.NewFilterBank(40, uint(*Bufsize))
-	fb.SetMelCoeffsSlaney(uint(*Samplerate))
-
-	inputBuffer := aubio.NewSimpleBufferData(uint(*Bufsize), buffer)
-	pitch.Do(inputBuffer)
-	pitch_val := pitch.Buffer().Slice()[0]
-
-	phVoc.Do(inputBuffer)
-	fftgrain := phVoc.Grain()
-	fb.Do(fftgrain)
-	return fb.Buffer().Slice(), pitch_val
+    inBuff = aubio.NewSimpleBufferData(uint(*Bufsize), buffer)
+    pitch.Do(inBuff)
+    phVoc.Do(inBuff)
+    fb.Do(phVoc.Grain())
+    return fb.Buffer().Slice(), pitch.Buffer().Slice()[0]
 }
